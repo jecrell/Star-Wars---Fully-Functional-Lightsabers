6aa2b9ca29743962e0e3d11e2caaa2ffc5353bf6
jecrell <matt.walls31@gmail.com>
2017-04-30 15:24:57 +0900

    Added interlocking Star Wars - Factions support
    Scum Bosses, Imperial Commanders, and Rebel Commanders now have chances
    to drop legendary lightsaber crystals.

--- Source/SWSaber/SWSaber_Utility.cs
+++ Source/SWSaber/SWSaber_Utility.cs
@@ -10,24 +10,36 @@ namespace SWSaber
     {
         public static bool modCheck = false;
         public static bool loadedForcePowers = false;
+        public static bool loadedFactions = false;
 
         public static bool AreForcePowersLoaded()
         {
             if (!modCheck) ModCheck();
             return loadedForcePowers;
         }
+        public static bool AreFactionsLoaded()
+        {
+            if (!modCheck) ModCheck();
+            return loadedFactions;
+        }
 
         public static void ModCheck()
         {
             Log.Message("Mod Check Called");
             loadedForcePowers = false;
+            loadedFactions = false;
             foreach (ModContentPack ResolvedMod in LoadedModManager.RunningMods)
             {
-                if (loadedForcePowers) break; //Save some loading
+                if (loadedForcePowers && loadedFactions) break; //Save some loading
                 if (ResolvedMod.Name.Contains("Star Wars - The Force"))
                 {
                     Log.Message("Lightsabers :: Star Wars - The Force Detected.");
                     loadedForcePowers = true;
+                }
+                if (ResolvedMod.Name.Contains("Star Wars - Factions"))
+                {
+                    Log.Message("Lightsabers :: Star Wars - Factions Detected.");
+                    loadedFactions = true;
                 }
             }
             modCheck = true;
