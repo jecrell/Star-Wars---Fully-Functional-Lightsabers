7b70bc47426d296bf9e6c69574c294bdb2b3d55e
erdelf <erdelf.productions@gmail.com>
2017-05-20 18:20:15 +0200

    lightsaber glow

--- Source/SWSaber/CompProperties_LightsaberActivatableEffect.cs
+++ Source/SWSaber/CompProperties_LightsaberActivatableEffect.cs
@@ -1,7 +1,4 @@
 ﻿using CompActivatableEffect;
-using System;
-using System.Collections.Generic;
-using Verse;
 
 namespace SWSaber
 {
---
+++ Source/SWSaber/SaberGlow.cs
@@ -0,0 +1,36 @@
+﻿using Verse;
+using Harmony;
+using System.Threading;
+using System;
+
+namespace SWSaber
+{
+    class SaberGlow : CompGlower
+    {
+        Timer t;
+        public override void PostSpawnSetup(bool respawningAfterLoad)
+        {
+            Traverse.Create(this).Field("glowOnInt").SetValue(true);
+            this.parent.MapHeld.glowGrid.RegisterGlower(this);
+
+            this.t = new Timer(this.GlowTick, null, 200, 100);
+        }
+
+        IntVec3 pos = IntVec3.Invalid;
+
+        public void GlowTick(object state)
+        {
+            try
+            {
+                this.parent.Map.glowGrid.MarkGlowGridDirty(this.parent.Position);
+            } catch(Exception ex) { Log.Error(ex.Message + "\n" + ex.StackTrace); }
+        }
+
+        public override void PostDestroy(DestroyMode mode, Map previousMap)
+        {
+            this.t.Dispose();
+            this.parent.Map.glowGrid.DeRegisterGlower(this);
+            base.PostDestroy(mode, previousMap);
+        }
+    }
+}
