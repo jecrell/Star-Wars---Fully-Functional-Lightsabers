9e45c2dd2f25bfce7a69ce9fcef80ef5bc90f9f3
jecrell <matt.walls31@gmail.com>
2017-05-24 02:09:59 +0900

    A17 - Now dependant on JecsTools

--- Source/Properties/AssemblyInfo.cs
+++ Source/Properties/AssemblyInfo.cs
@@ -14,5 +14,5 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("a8e081b6-8768-4fe9-817f-f0ee42cf13ef")]
 
-[assembly: AssemblyVersion("1.17.0.1")]
-[assembly: AssemblyFileVersion("1.17.0.1")]
+[assembly: AssemblyVersion("1.17.0.3")]
+[assembly: AssemblyFileVersion("1.17.0.3")]
